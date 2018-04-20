d6d3659f9c2fbee513ddc818d3ecf164ca8f3808
Andy Driver <andy@pagezero.net>
2018-04-20 18:05:48 +0100

    Put secrets in team namespace

--- charts/concourse-org-pipeline/files/set-pipeline.sh
+++ charts/concourse-org-pipeline/files/set-pipeline.sh
@@ -14,14 +14,10 @@ fly -t default set-pipeline \
     -n $CONCOURSE_TEAM \
     -p $1 \
     -c /scripts/pipeline.yaml \
-    -v concourse-team=$CONCOURSE_TEAM \
-    -v concourse-url=$CONCOURSE_URL \
-    -v fly-username=$FLY_USERNAME \
-    -v fly-password=$FLY_PASSWORD \
+    -v team_name=$CONCOURSE_TEAM \
+    -v concourse_url=$CONCOURSE_URL \
     -v github-access-token=$GITHUB_ACCESS_TOKEN \
     -v github-org=$1 \
     -v github-org-resource-image=$GITHUB_ORG_RESOURCE_IMAGE \
     -v github-org-resource-tag=$GITHUB_ORG_RESOURCE_TAG \
-    -v github-webhook-token=$GITHUB_WEBHOOK_TOKEN \
-    -v kubernetes-ca-cert=$KUBERNETES_CA_CERT \
-    -v kubernetes-token=$KUBERNETES_TOKEN
+    -v github-webhook-token=$GITHUB_WEBHOOK_TOKEN
